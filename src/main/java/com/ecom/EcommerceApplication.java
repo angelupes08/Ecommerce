02d5b5d9b7d0ad25f96159906abc78178c2b4c63
angelupes08 <48271087+angelupes08@users.noreply.github.com>
2024-10-31 13:59:52 +0530

    Update EcommerceApplication.java

--- src/main/java/com/ecom/EcommerceApplication.java
+++ src/main/java/com/ecom/EcommerceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableCaching
+@EnableCaching //used for redis caching
 public class EcommerceApplication {
 
     public static void main(String[] args) {
